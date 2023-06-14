c8121096c217f619f6ec4366b1b2fc12ee6ae1c0
nestordqa <ndqa96@gmail.com>
2023-06-14 00:19:06 -0400

    fixing contact form

--- src/render/Contact.jsx
+++ src/render/Contact.jsx
@@ -32,196 +32,202 @@ const onSubmit = async (data) => {
   return (
     <div className={styles.container}>
 
-    <div className={styles.header}>
-      <img src={logo} alt="Three Columns Logo" className={styles.logo}/>
-      <div>
-        <span>
-          We&apos;re <br/> building.
-        </span>
-      </div>
-    </div>
-
-    <div className={styles.content}>
-
-      <div className={styles.building}>
-        <span>
-          Let&apos;s talk!
-        </span>
+      <div className={styles.header}>
+        <img src={logo} alt="Three Columns Logo" className={styles.logo}/>
+        <div>
+          <span>
+            We&apos;re <br/> building.
+          </span>
+        </div>
       </div>
 
-      <div className={styles.contact}>
-        <form ref={form} onSubmit={handleSubmit(onSubmit)}>
-          <div className={styles.div1}>
+      <div className={styles.content}>
 
-            <div className="form__group field">
-              <input 
-                className="form__field" 
-                placeholder="Name" 
-                name='name'
-                {...register('name', {
-                  required: true,
-                  minLength: 4
-                  })
-                }
-              />
-              {errors.name?.type === 'minLength' ? (
-                <p className="error">
-                  You must write a larger name.
-                </p>
-                ) 
-                : 
-                null
-              }
-              {errors.name?.type === 'required' ? (
-                <p className="error">
-                  Field required
-                </p>
-                ) 
-                : 
-                null
-              }
-              <label htmlFor="name" className="form__label">What's your name?</label>
-            </div>
-            <div className="form__group field">
-              <input 
-                className="form__field" 
-                placeholder="Name" 
-                name='email'
-                {...register('email', {
-                  required: true,
-                  })
-                }
-              />
-              {errors.email?.type === 'required' ? (
-                <p className="error">
-                  Field required
-                </p>
-                ) 
-                : 
-                null
-              }
-              <label htmlFor="email" className="form__label">What's your email?</label>
-            </div>
-            <div className="form__group field">
-              <input 
-                className="form__field" 
-                placeholder="Name" 
-                name="makeforyou" 
-                {...register('makeforyou', {
-                  required: true,
-                  maxLength: 50
-                })
-                }
-              />
-              {errors.makeforyou?.type === 'required' ? (
-                <p className="error">
-                  Field required
-                </p>
-                ) 
-                : 
-                null
-              }
-              {errors.makeforyou?.type === 'maxLength' ? (
-                <p className="error">
-                  You must write a shorter sentence.
-                </p>
-                ) 
-                : 
-                null
-              }
-              <label htmlFor="makeforyou" className="form__label">What can we make for you?</label>
-            </div>
+        <div className={styles.building}>
+          <span>
+            Let&apos;s talk!
+          </span>
+        </div>
 
-          </div>
-          <div className={styles.div2}>
+        <div className={styles.contact}>
+          <form ref={form} onSubmit={handleSubmit(onSubmit)}>
+            <div className={styles.div1}>
 
-            <div className="form__group field">
+              <div>
+                <label htmlFor="name" className={styles.label}>What's your name?</label>
                 <input 
-                  className="form__field" 
-                  placeholder="Name"
+                  className={styles.input} 
+                  name='name'
+                  {...register('name', {
+                    required: true,
+                    minLength: 4
+                    })
+                  }
+                />
+                {errors.name?.type === 'minLength' ? (
+                  <p className="error">
+                    You must write a larger name.
+                  </p>
+                  ) 
+                  : 
+                  null
+                }
+                {errors.name?.type === 'required' ? (
+                  <p className="error">
+                    Field required
+                  </p>
+                  ) 
+                  : 
+                  null
+                }
+              </div>
+              
+              <div>
+                <label htmlFor="email" className={styles.label}>What's your email?</label>
+                <input 
+                  className={styles.input}  
+                  name='email'
+                  {...register('email', {
+                    required: true,
+                    })
+                  }
+                />
+                {errors.email?.type === 'required' ? (
+                  <p className="error">
+                    Field required
+                  </p>
+                  ) 
+                  : 
+                  null
+                }
+              </div>
+              <div>
+                <label htmlFor="makeforyou" className={styles.label}>What can we make for you?</label>
+                <input 
+                  className={styles.input} 
+                  name="makeforyou" 
+                  {...register('makeforyou', {
+                    required: true,
+                    maxLength: 50
+                  })
+                  }
+                />
+                {errors.makeforyou?.type === 'required' ? (
+                  <p className="error">
+                    Field required
+                  </p>
+                  ) 
+                  : 
+                  null
+                }
+                {errors.makeforyou?.type === 'maxLength' ? (
+                  <p className="error">
+                    You must write a shorter sentence.
+                  </p>
+                  ) 
+                  : 
+                  null
+                }
+              </div>
+
+            </div>
+            <div className={styles.div2}>
+
+              <div>
+                <label htmlFor="budget" className={styles.label}>What's your budget?</label>
+                <input 
+                  className={styles.input} 
                   name='budget' 
                   {...register('budget', {
                     required: true,
                     maxLength: 20
                     })
                   }
-                />
-                {errors.budget?.type === 'required' ? (
-                <p className="error">
-                  Field required
-                </p>
-                ) 
-                : 
-                null
-              }
-              <label htmlFor="budget" className="form__label">What's your budget?</label>
-            </div>
-            <div className="form__group field">
-              <textarea 
-                  name='description'
-                  {...register('description', {
-                  required: true,
-                  minLength: 100
-                  })
+                  />
+                  {errors.budget?.type === 'required' ? (
+                  <p className="error">
+                    Field required
+                  </p>
+                  ) 
+                  : 
+                  null
                 }
-              />
-              {errors.description?.type === 'required' ? (
-                <p className="error">
-                  Field required
+                {errors.budget?.type === 'maxLength' ? (
+                  <p className="error">
+                    You must write a shorter sentence.
+                  </p>
+                  ) 
+                  : 
+                  null
+                }                
+              </div>
+              <div>
+                <label htmlFor="talk" className={styles.label}>Describe your project in detail:</label>
+                <textarea 
+                    className={styles.textarea}
+                    name='description'
+                    {...register('description', {
+                    required: true,
+                    minLength: 100
+                    })
+                  }
+                />
+                {errors.description?.type === 'required' ? (
+                  <p className="error">
+                    Field required
+                  </p>
+                  ) 
+                  : 
+                  null
+                }
+                {errors.description?.type === 'minLength' ? (
+                  <p className="error">
+                    Your project description must have 100 characters at least.
+                  </p>
+                  ) 
+                  : 
+                  null
+                }
+              </div>
+
+            </div>
+            <div className={styles.div3}>
+
+              <button type='submit'>
+                Get in touch!
+                <span className="first"></span>
+                <span className="second"></span>
+                <span className="third"></span>
+                <span className="fourth"></span>
+              </button>
+              <div className={styles.working}>
+                <span>
+                  We&apos;re working on our new website.
+                </span>
+              </div>
+              <div className={styles.copyright}>
+                <p>
+                  All rights reserved &#169; 2023.
                 </p>
-                ) 
-                : 
-                null
-              }
-              {errors.description?.type === 'minLength' ? (
-                <p className="error">
-                  Your project description must have 100 characters at least.
+              </div>
+              <div className={styles.foot}>
+                <span>
+                  We&apos;re building.
+                </span>
+                <br/>
+                <span>
+                  We&apos;re working on our new website.
+                </span>
+                <p>
+                  All rights reserved &#169; 2023.
                 </p>
-                ) 
-                : 
-                null
-              }
-              <label htmlFor="talk" className="form__label">Describe your project in detail:</label>
+              </div>
             </div>
 
-          </div>
-          <div className={styles.div3}>
+          </form>
 
-            <button type='submit'>
-              Get in touch!
-              <span className="first"></span>
-              <span className="second"></span>
-              <span className="third"></span>
-              <span className="fourth"></span>
-            </button>
-            <div className={styles.working}>
-              <span>
-                We&apos;re working on our new website.
-              </span>
-            </div>
-            <div className={styles.copyright}>
-              <p>
-                All rights reserved &#169; 2023.
-              </p>
-            </div>
-            <div className={styles.foot}>
-              <span>
-                We&apos;re building.
-              </span>
-              <br/>
-              <span>
-                We&apos;re working on our new website.
-              </span>
-              <p>
-                All rights reserved &#169; 2023.
-              </p>
-            </div>
-          </div>
-
-        </form>
-
+        </div>
       </div>
-    </div>
   </div>
   )
 };
