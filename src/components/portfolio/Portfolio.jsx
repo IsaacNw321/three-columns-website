6cf9e91344a91067b251cb6a2ecefb6620b642cc
nestordqa <ndqa96@gmail.com>
2023-07-16 17:03:05 -0400

    fix: fixing navbar styles

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Layout } from '../layout/Layout';
+import { Projects } from './Projects';
 
 export const Portfolio = () => {
   return (
     <Layout>
-        
+        <Projects />
     </Layout>
     )
 }
