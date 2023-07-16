1b84c1bd956cf7e06d9e8b0b98e4367d6335f53a
nestordqa <ndqa96@gmail.com>
2023-07-16 17:36:02 -0400

    feat: porftolio page already working

--- src/components/portfolio/Projects.jsx
+++ src/components/portfolio/Projects.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
 import styles from "../../styles/portfolio.module.css";
+import img from "../../images/branding.jpg";
 
 export const Projects = () => {
   return (
-    <div className={styles.container}>Projects</div>
+    <div className={styles.container}>
+        <div className={styles.info}>
+            <h2>
+                Project name
+            </h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Et alias dignissimos sunt dolorem nam unde, possimus distinctio odio tempore sapiente eaque molestiae libero, eius numquam voluptate nemo ea atque sequi.
+            </p>
+            <span>
+                01. Text
+            </span>
+        </div>
+        <div className={styles.firstPhoto}>
+            <img src={img} alt="First projecte reference"/>
+        </div>
+        <div className={styles.secondPhoto}>
+            <img src={img} alt="Second projecte reference"/>
+        </div>
+    </div>
   )
 }
